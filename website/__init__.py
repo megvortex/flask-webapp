82c722a86413345f2d890417bc21d04f9c963930
meggyn <meggyn1999@gmail.com>
2025-08-11 16:29:33 +0700

    register & login page

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '21suliga'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
@@ -10,4 +17,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
-    return app
+    with app.app_context():
+        from .models import User, Note
+        db.create_all()
+    return app 
--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', text="This is the Login Page - Test", boolean=False)
+    data = request.form
+    if request.method == 'POST':
+        print(data)
+    return render_template('login.html', text="This is the Login Page - Test")
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fullname = request.form.get('fullname')
+        password = request.form.get('password') 
+        confirm_password = request.form.get('confirm_password')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(fullname) < 6:
+            flash('Full name must be greater than 5 characters', category='error')
+        elif password != confirm_password:
+            flash('Passwords do not match', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 6 characters', category='error')
+        else:
+            new_user = User(email=email, fullname=fullname, password=password)
+            flash('Account created!', category='success')
+
     return render_template('register.html')
 
 @auth.route('/logout')
